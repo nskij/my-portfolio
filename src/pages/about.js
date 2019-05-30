ee91900ab2448bd660f5268c0ec65d06aa756dc2
nskij <nskij0123@gmail.com>
2019-05-30 23:35:00 +0900

    仮置きabout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import '../App.css';
+import ProfileCard from '../components/profileCard';
 
 export default class About extends Component {
   render() {
     return(
-      <div>
-        <p>nskijの経歴やスキルについてです。</p>
-      </div>
+      <React.Fragment>
+        <div className = "card-container">
+          <ProfileCard/>
+        </div>
+      </React.Fragment>
+
     );
   }
 }
