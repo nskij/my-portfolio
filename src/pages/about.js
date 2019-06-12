ca675d42fd1125c94cba1aec6562ec48fc3776e9
nskij <nskij0123@gmail.com>
2019-06-12 19:25:16 +0900

    aboutリファクタリング

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import AboutCard from '../components/aboutCard';
+import Typography from '@material-ui/core/Typography';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 function About() {
+  const useStyles = makeStyles({
+    card: {
+      minWidth: 200,
+      maxWidth: 400
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+  const classes = useStyles();
   return(
-      <React.Fragment>
-        <div>
-          <AboutCard/>
-        </div>
-      </React.Fragment>
+    <React.Fragment>
+        <Card className={classes.card}>
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              About @nskij
+            </Typography>
+            <br />
+            <Typography className={classes.pos} color="textSecondary">
+              birthday: 1993/05/26
+              <br />
+              GitHub: <a href="https://github.com/nskij">https://github.com/nskij</a>
+              <br />
+              hatena: <a href="https://nskij.hatenablog.com/">https://nskij.hatenablog.com/</a>
+              <br />
+              Qiita: <a href="https://qiita.com/nskij">https://qiita.com/nskij</a>
+              <br />
+              Diary: <a href="https://stella-app.jp/user?id=bVswE3Q69yYBtmlJm7VvUyD1bIl1">https://stella-app.jp</a>
+            </Typography>
+          </CardContent>
+        </Card>
+    </React.Fragment>
   );
 }
 
